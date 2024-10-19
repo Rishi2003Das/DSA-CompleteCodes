8748e612c141f749989cbc7bf5068e46a6a51f36
Rishi Das <113520748+Rishi2003Das@users.noreply.github.com>
2024-10-19 10:39:50 +0530

    Update AVLTree.java

--- AVLTree.java
+++ AVLTree.java
@@ -162,3 +162,17 @@ class AVL {
   }
 
 }
+
+
+
+class AVLMain {
+  public static void main(String[] args) {
+    AVL tree = new AVL();
+
+    for(int i=0; i < 1000; i++) {
+      tree.insert(i);
+    }
+
+    System.out.println(tree.height());
+  }
+}
