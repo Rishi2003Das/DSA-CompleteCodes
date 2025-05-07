b0e605b35705bd0c5b277206c230d827b3874658
Rishi Das <113520748+Rishi2003Das@users.noreply.github.com>
2025-05-07 13:14:57 +0530

    Update Alien Dictionary.java

--- Alien Dictionary.java
+++ Alien Dictionary.java
@@ -1,51 +1,52 @@
-//This is a TopoSort algorithm used pattern prediction question
+//This is a TopoSort algorithm used for the pattern prediction question
 
 class Solution {
-    public String findOrder(String [] dict, int N, int K) {
-        List<List<Integer>> adj=new ArrayList<Integer>();
-        for(int i=0;i<N-1;i++){
-            String p=dict[i];
-            String q=dict[i+1];
-            int m=p.length();
-            int n=q.lenght();
-            int c=0;
-            while(m>0 && n>0){
-                if(p.charAt(c)!=q.charAt(c)){
-                    int a=(int)p.charAt(c)-97;
-                    int b=(int)q.charAt(c)-97;
+    public String findOrder(String[] dict, int N, int K) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < K; i++) {
+            adj.add(new ArrayList<>()); // Initialize adjacency list for each character
+        }
+
+        // Build the graph
+        for (int i = 0; i < N - 1; i++) {
+            String p = dict[i];
+            String q = dict[i + 1];
+            int minLen = Math.min(p.length(), q.length());
+            for (int c = 0; c < minLen; c++) {
+                if (p.charAt(c) != q.charAt(c)) {
+                    int a = p.charAt(c) - 'a';
+                    int b = q.charAt(c) - 'a';
                     adj.get(a).add(b);
-                    break; 
+                    break;
                 }
-                m--;
-                n--;
-                c++;
             }
         }
-        return topoSort(K,adj);
+
+        // Get topological sort
+        return topoSort(K, adj);
     }
-    public char[] topoSort(int V, List<List<Integer>> adj) {
-    int vis[]=new int[V];
-    int ans[]=new int[V];
-    char fin[]=new char[V];
-    Stack<Integer> st=new Stack<>();
-    for(int i=0;i<V; i++){
-        if(vis[i]==0)
-        dfs(i, vis, st, adj);
+
+    public String topoSort(int V, List<List<Integer>> adj) {
+        int[] vis = new int[V];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < V; i++) {
+            if (vis[i] == 0)
+                dfs(i, vis, st, adj);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!st.isEmpty()) {
+            sb.append((char)(st.pop() + 'a'));
+        }
+        return sb.toString();
     }
-    for(int i=0;i<V;i++){
-        ans[i]=st.peek();
-        st.pop();
-    }
-    for(int i=0;i<V;i++){
-        fin[i]=(char)(ans[i]+97);
-    }
-    return fin;
-    }
-    public static void dfs(int node, int vis[], Stack<Integer> st, List<List<Integer>> adj){
-        vis[node]=1;
-        for(int it : adj.get(node)){
-            if(vis[it]==0)
-            dfs(it,vis,st,adj);
+
+    public void dfs(int node, int[] vis, Stack<Integer> st, List<List<Integer>> adj) {
+        vis[node] = 1;
+        for (int it: adj. get(node)) {
+            if (vis[it] == 0)
+                dfs(it, vis, st, adj);
         }
         st.push(node);
     }
