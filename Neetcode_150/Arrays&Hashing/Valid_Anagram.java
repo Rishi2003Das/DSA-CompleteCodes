5c0bd0e5247bf19aa5e01efc734fde53b05d5fae
Rishi Das <113520748+Rishi2003Das@users.noreply.github.com>
2025-08-29 22:00:00 +0530

    Create Valid_Anagram.java

---
+++ Neetcode_150/Arrays&Hashing/Valid_Anagram.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] count = new int[26];
+        
+        // Count the frequency of characters in string s
+        for (char x : s.toCharArray()) {
+            count[x - 'a']++;
+        }
+        
+        // Decrement the frequency of characters in string t
+        for (char x : t.toCharArray()) {
+            count[x - 'a']--;
+        }
+        
+        // Check if any character has non-zero frequency
+        for (int val : count) {
+            if (val != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
