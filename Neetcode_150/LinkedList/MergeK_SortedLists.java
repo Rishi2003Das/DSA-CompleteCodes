400e8eafa7c63a5dd18b45e3da09da820d5babf4
Rishi Das <113520748+Rishi2003Das@users.noreply.github.com>
2025-10-08 21:51:19 +0530

    Create MergeK_SortedLists.java

---
+++ Neetcode_150/LinkedList/MergeK_SortedLists.java
@@ -0,0 +1,141 @@
+//Brute Force
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+public class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        List<Integer> nodes = new ArrayList<>();
+        for (ListNode lst : lists) {
+            while (lst != null) {
+                nodes.add(lst.val);
+                lst = lst.next;
+            }
+        }
+        Collections.sort(nodes);
+
+        ListNode res = new ListNode(0);
+        ListNode cur = res;
+        for (int node : nodes) {
+            cur.next = new ListNode(node);
+            cur = cur.next;
+        }
+        return res.next;
+    }
+}
+//Optimised Solution
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) return null;
+
+        for (int i = 1; i < lists.length; i++) {
+            lists[i] = merge(lists[i], lists[i - 1]);
+        }
+        return lists[lists.length - 1];
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+
+            curr = curr.next;
+        }
+
+        if (l1 != null) {
+            curr.next = l1;
+        } else {
+            curr.next = l2;
+        }
+
+        return dummy. Next;
+    }
+}
+//Divide and Conquer(Most Efficient)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        return divide(lists, 0, lists.length - 1);
+    }
+
+    private ListNode divide(ListNode[] lists, int l, int r) {
+        if (l > r) {
+            return null;
+        }
+        if (l == r) {
+            return lists[l];
+        }
+
+        int mid = l + (r - l) / 2;
+        ListNode left = divide(lists, l, mid);
+        ListNode right = divide(lists, mid + 1, r);
+
+        return conquer(left, right);
+    }
+
+    private ListNode conquer(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+
+            curr = curr.next;
+        }
+
+        if (l1 != null) {
+            curr.next = l1;
+        } else {
+            curr.next = l2;
+        }
+
+        return dummy.next;
+    }
+}
