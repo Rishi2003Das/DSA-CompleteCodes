c64964f251f67695415e98d8c73ce6aaf9706f53
Rishi Das <113520748+Rishi2003Das@users.noreply.github.com>
2026-01-26 09:31:28 +0530

    Create ClimbingStairs.java

---
+++ Neetcode_150/DP/ClimbingStairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2) return n;
+
+        int prev2 = 1;
+        int prev1 = 2;
+        for (int i = 3; i <= n; i++) {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+}
