2efdd90eba1d4f5f7181d59a9d3e864cfdcfc45b
Rishi Das <113520748+Rishi2003Das@users.noreply.github.com>
2026-02-12 12:17:05 +0530

    Add problem description for Word Search
    Added a docstring explaining the Word Search problem.

--- WordSearch.java
+++ WordSearch.java
@@ -1,3 +1,12 @@
+/*
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally
+or vertically neighbouring. The same letter cell may not be used more than once.
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+Output: true
+*/
+
 class Solution {
     public boolean exist(char[][] board, String word) {
         int m=board.length;
