eae9b3f33b4885cfc9a9d2e0d37af7144820b5d9
Rishi Das <113520748+Rishi2003Das@users.noreply.github.com>
2026-02-12 12:08:46 +0530

    Refactor combinationSum and makeCombination methods

--- Neetcode_150/Backtracking/CombinationSum.java
+++ Neetcode_150/Backtracking/CombinationSum.java
@@ -26,23 +26,23 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
 
-        makeCombination(candidates, target, 0, new ArrayList<>(), 0, res);
+        makeCombination(candidates, target, 0, new ArrayList<>(), res);
         return res;        
     }
 
-    private void makeCombination(int[] candidates, int target, int idx, List<Integer> comb, int total, List<List<Integer>> res) {
-        if (total == target) {
+    private void makeCombination(int[] candidates, int count, int idx, List<Integer> comb, List<List<Integer>> res) {
+        if (count == 0) {
             res.add(new ArrayList<>(comb));
             return;
         }
 
-        if (total > target || idx >= candidates.length) {
+        if (count<0 || idx >= candidates.length) {
             return;
         }
 
         comb.add(candidates[idx]);
-        makeCombination(candidates, target, idx, comb, total + candidates[idx], res);
+        makeCombination(candidates, count-candidates[idx], idx, comb, res);
         comb.remove(comb.size() - 1);
-        makeCombination(candidates, target, idx + 1, comb, total, res);
+        makeCombination(candidates, count, idx + 1, comb, res);
     }    
 }
