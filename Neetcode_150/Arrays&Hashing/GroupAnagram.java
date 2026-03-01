fc73a77dd4df58f42612ddd23499af7cacd8426a
Rishi Das <113520748+Rishi2003Das@users.noreply.github.com>
2026-03-01 09:40:42 +0530

    Add problem description for Group Anagram
    Added a comment block explaining the Group Anagram problem.

--- Neetcode_150/Arrays&Hashing/GroupAnagram.java
+++ Neetcode_150/Arrays&Hashing/GroupAnagram.java
@@ -1,3 +1,12 @@
+/*
+49. Group Anagram
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+Example 1:
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+*/
+
+
 public class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> res = new HashMap<>();
