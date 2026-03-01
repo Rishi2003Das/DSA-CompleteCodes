2db24fc6d88984a23d6a05e10f3b8759f4e02495
Rishi Das <113520748+Rishi2003Das@users.noreply.github.com>
2026-03-01 09:31:16 +0530

    Update Duplicates.java

--- Neetcode_150/Arrays&Hashing/Duplicates.java
+++ Neetcode_150/Arrays&Hashing/Duplicates.java
@@ -1,3 +1,7 @@
+/*
+217. Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+*/
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         
