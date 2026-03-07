de9fb5fdebffec0a2508b509984f4e941196b04c
Rishi Das <113520748+Rishi2003Das@users.noreply.github.com>
2026-03-07 11:51:55 +0530

    Add method to generate subsets with duplicates

--- Neetcode_150/Backtracking/Subset.java
+++ Neetcode_150/Backtracking/Subset.java
@@ -50,3 +50,31 @@ public class Solution {
         return res;
     }
 }
+
+//With Duplication
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> subset = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(0, nums, subset, res);
+        return res;        
+    }
+
+    private void backtrack(int i, int[] nums, List<Integer> subset, List<List<Integer>> res) {
+        if (i == nums.length) {
+            res.add(new ArrayList<>(subset));
+            return;
+        }
+
+        subset.add(nums[i]);
+        backtrack(i + 1, nums, subset, res);
+        subset.remove(subset.size() - 1);
+
+        while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
+            i++;
+        }
+
+        backtrack(i + 1, nums, subset, res);
+    }    
+}
