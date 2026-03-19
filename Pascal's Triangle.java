4bb637679f74c5d3e961005235fa2e1ef4799a89
Rishi Das <113520748+Rishi2003Das@users.noreply.github.com>
2026-03-20 01:04:00 +0530

    Implement method to generate Pascal's Triangle row

---
+++ Pascal's Triangle.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+        row.add(1);
+
+        for (int i = 0; i < rowIndex; i++) {
+            List<Integer> newRow = new ArrayList<>();
+            newRow.add(1);
+            for (int j = 1; j < row.size(); j++) {
+                newRow.add(row.get(j - 1) + row.get(j));
+            }
+            newRow.add(1);
+            row = newRow;
+        }
+
+        return row;        
+    }
+}
