5004294beb51cdac427ea3f8e2c23ec9c58a168b
Rishi Das <113520748+Rishi2003Das@users.noreply.github.com>
2026-03-25 14:53:59 +0530

    Add Twitter class with tweet and follow functionalities
    Implements a simplified version of Twitter with functionalities to post tweets, follow/unfollow users, and retrieve the user's news feed.

---
+++ Neetcode_150/Heaps/Design_Twitter.java
@@ -0,0 +1,114 @@
+/*
+355. Design Twitter-> Design a simplified version of Twitter where users can post tweets, follow/unfollow another user,
+and is able to see the 10 most recent tweets in the user's news feed.
+
+Implement the Twitter class:
+Twitter(): Initializes your twitter object.
+void postTweet(int userId, int tweetId): Composes a new tweet with ID tweetId by the user userId.
+Each call to this function will be made with a unique tweetId.
+List<Integer> getNewsFeed(int userId): Retrieves the 10 most recent tweet IDs in the user's news feed.
+Each item in the news feed must be posted by users who the user followed or by the user themself.
+Tweets must be ordered from most recent to least recent.
+void follow(int followerId, int followeeId): The user with ID followerId started following the user with ID followeeId.
+void unfollow(int followerId, int followeeId): The user with ID followerId started unfollowing the user with ID followeeId.
+ 
+
+Example 1:
+
+Input
+["Twitter", "postTweet", "getNewsFeed", "follow", "postTweet", "getNewsFeed", "unfollow", "getNewsFeed"]
+[[], [1, 5], [1], [1, 2], [2, 6], [1], [1, 2], [1]]
+Output
+[null, null, [5], null, null, [6, 5], null, [5]]
+
+Explanation
+Twitter twitter = new Twitter();
+twitter.postTweet(1, 5); // User 1 posts a new tweet (id = 5).
+twitter.getNewsFeed(1);  // User 1's news feed should return a list with 1 tweet id -> [5]. return [5]
+twitter.follow(1, 2);    // User 1 follows user 2.
+twitter.postTweet(2, 6); // User 2 posts a new tweet (id = 6).
+twitter.getNewsFeed(1);  // User 1's news feed should return a list with 2 tweet ids -> [6, 5]. 
+Tweet id 6 should precede tweet id 5 because it is posted after tweet id 5.
+twitter.unfollow(1, 2);  // User 1 unfollows user 2.
+twitter.getNewsFeed(1);  // User 1's news feed should return a list with 1 tweet id -> [5],
+since user 1 is no longer following user 2.
+*/
+
+class Twitter {
+    // map to store user's tweets (timestamp, tweetId)
+    HashMap<Integer, List<int[]>> tweets;
+
+    // map to store who follows whom
+    HashMap<Integer, Set<Integer>> following;
+
+    // timer count to give each tweet a unique timestamp
+    int time;
+
+    public Twitter() {
+        tweets = new HashMap<>();
+        following = new HashMap<>();
+        time = 0;
+    }
+    
+    public void postTweet(int userId, int tweetId) {
+        tweets.putIfAbsent(userId, new ArrayList<>());
+        tweets.get(userId).add(new int[] {time++, tweetId});
+    }
+    
+    public List<Integer> getNewsFeed(int userId) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+
+        // Add own tweets
+        if(tweets.containsKey(userId)) {
+            for(int[] tweet : tweets.get(userId)) {
+                pq.offer(tweet);
+                if(pq.size() > 10) {
+                    pq.poll();
+                }
+            }
+        }
+
+        // Add tweets from followed users
+        if(following.containsKey(userId)) {
+            for(int followee : following.get(userId)) {
+                if(tweets.containsKey(followee)) {
+                    for(int[] tweet : tweets.get(followee)) {
+                        pq.offer(tweet);
+                        if(pq.size() > 10) {
+                            pq.poll();
+                        }
+                    }
+                }
+            }
+        }
+
+        LinkedList<Integer> list = new LinkedList<>();
+        while(!pq.isEmpty()) {
+            list.addFirst(pq.poll()[1]);
+        }
+
+        return list;
+    }
+    
+    public void follow(int followerId, int followeeId) {
+        following.putIfAbsent(followerId, new HashSet<>());
+        following.get(followerId).add(followeeId);
+    }
+    
+    public void unfollow(int followerId, int followeeId) {
+        if(following.containsKey(followerId)) {
+            following.get(followerId).remove(followeeId);
+        }
+    }
+}
+
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter obj = new Twitter();
+ * obj.postTweet(userId,tweetId);
+ * List<Integer> param_2 = obj.getNewsFeed(userId);
+ * obj.follow(followerId,followeeId);
+ * obj.unfollow(followerId,followeeId);
+ */ 
+
