50342f447133419521e52a8f56947547e84f1e77
Rishi Das <113520748+Rishi2003Das@users.noreply.github.com>
2026-04-04 19:59:23 +0530

    Add MedianFinder class for streaming median calculation
    Implement the MedianFinder class to calculate the median from a data stream.

---
+++ Neetcode_150/Heaps/Median_of_DataStream.java
@@ -0,0 +1,70 @@
+/*
+295. Median from Data Stream:
+The median is the middle value in an ordered integer list. If the 
+size of the list is even, there is no middle value, and the 
+median is the mean of the two middle values.
+
+For example, for arr = [2,3,4], the median is 3.
+For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+Implement the MedianFinder class:
+
+MedianFinder() initialises the MedianFinder object.
+void addNum(int num) adds the integer num from the data stream to the data structure.
+double findMedian() returns the median of all elements so far. 
+Answers within 10-5 of the actual answer will be accepted.
+
+Example 1:
+Input
+["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+[[], [1], [2], [], [3], []]
+Output
+[null, null, null, 1.5, null, 2.0]
+Explanation
+MedianFinder medianFinder = new MedianFinder();
+medianFinder.addNum(1);    // arr = [1]
+medianFinder.addNum(2);    // arr = [1, 2]
+medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+medianFinder.addNum(3);    // arr[1, 2, 3]
+medianFinder.findMedian(); // return 2.0
+*/
+
+import java.util.*;
+
+class MedianFinder {
+    private PriorityQueue<Integer> lowerHalf; // max-heap
+    private PriorityQueue<Integer> upperHalf; // min-heap
+
+    public MedianFinder() {
+        lowerHalf = new PriorityQueue<>(Collections.reverseOrder());
+        upperHalf = new PriorityQueue<>();
+    }
+
+    public void addNum(int num) {
+        if (lowerHalf.isEmpty() || num <= lowerHalf.peek()) {
+            lowerHalf.offer(num);
+        } else {
+            upperHalf.offer(num);
+        }
+
+        // Balance the heaps
+        if (lowerHalf.size() > upperHalf.size() + 1) {
+            upperHalf.offer(lowerHalf.poll());
+        } else if (upperHalf.size() > lowerHalf.size()) {
+            lowerHalf.offer(upperHalf.poll());
+        }
+    }
+
+    public double findMedian() {
+        if (lowerHalf.size() > upperHalf.size()) {
+            return lowerHalf.peek();
+        }
+        return (lowerHalf.peek() + upperHalf.peek()) / 2.0;
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
