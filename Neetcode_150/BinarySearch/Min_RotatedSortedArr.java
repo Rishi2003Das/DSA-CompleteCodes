453377c6f5c296d5d78f767954cc1f5d036522ea
Rishi Das <113520748+Rishi2003Das@users.noreply.github.com>
2026-05-15 08:03:22 +0530

    Update Min_RotatedSortedArr.java

--- Neetcode_150/BinarySearch/Min_RotatedSortedArr.java
+++ Neetcode_150/BinarySearch/Min_RotatedSortedArr.java
@@ -1,9 +1,23 @@
+/*
+Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+[4,5,6,7,0,1,2] if it was rotated 4 times.
+[0,1,2,4,5,6,7] if it was rotated 7 times.
+Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+Given the sorted, rotated array nums of unique elements, return the minimum element of this array.
+You must write an algorithm that runs in O(log n) time.
+
+Example 1:
+Input: nums = [3,4,5,1,2]
+Output: 1
+Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+*/
+
+
 class Solution {
     public int findMin(int[] nums) {
         // First, get the index where the rotation happened
         int pivotIndex = getPivotIndex(nums);
 
-        // The number at pivot is the smallest — the poor thing was pushed to the front
         return nums[pivotIndex];
     }
 
@@ -28,7 +42,6 @@ class Solution {
             }
         }
 
-        // If you reach here, the array was never rotated. Lazy array. Just return 0.
         return 0;
     }
 }
