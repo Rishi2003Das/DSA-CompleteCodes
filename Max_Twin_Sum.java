c2c1b19280c31b25768f7add9049d634aca49299
Rishi Das <113520748+Rishi2003Das@users.noreply.github.com>
2026-06-14 23:51:38 +0530

    Add solution for maximum twin sum of linked list
    Implement solution to find maximum twin sum in a linked list.

---
+++ Max_Twin_Sum.java
@@ -0,0 +1,43 @@
+/*
+2130. Max Twin Sum of a Linked List:
+In a linked list of size n, where n is even, the ith node (0-indexed) of the 
+linked list is known as the twin of the (n-1-i)th node, if 0 <= i <= (n / 2) - 1.
+
+For example, if n = 4, then node 0 is the twin of node 3, and node 1 is the twin of node 2. 
+These are the only nodes with twins for n = 4.
+The twin sum is defined as the sum of a node and its twin.
+Given the head of a linked list with even length, return the maximum twin sum of the linked list.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int pairSum(ListNode head) {
+        ListNode slow = head, fast = head, prev = null;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            ListNode temp = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = temp;
+        }
+
+        int res = 0;
+        while (slow != null) {
+            res = Math.max(res, prev.val + slow.val);
+            prev = prev.next;
+            slow = slow.next;
+        }
+
+        return res;
+    }
+}
