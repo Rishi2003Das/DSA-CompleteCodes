7ea859ee583a0924e7ee39473f3ac556d9a7b8de
Rishi Das <113520748+Rishi2003Das@users.noreply.github.com>
2026-07-06 01:10:43 +0530

    Implement topological sort using DFS

---
+++ Neetcode_150/Graphs/TopoSort.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class Solution {
+    public void dfs(int cur, List<List<Integer>> adj, boolean[] visited, Stack<Integer> s) {
+        visited[cur] = true;
+        for(int k : adj.get(cur)) {
+            if(!visited[k]) {
+                dfs(k, adj, visited, s);
+            }
+        }
+        s.push(cur);
+    }
+
+    public List<Integer> topologicalSort(List<List<Integer>> adj) {
+        int n = adj.size();
+        boolean[] visited = new boolean[n];
+        Stack<Integer> s = new Stack<>();
+        
+        for(int i = 0; i < n; i++) {
+            if(!visited[i]) {
+                dfs(i, adj, visited, s);
+            }
+        }
+        
+        List<Integer> ans = new ArrayList<>();
+        while(!s.isEmpty()) {
+            ans.add(s.pop());
+        }
+        return ans;
+    }
+}
+
+/*public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int e = scanner.nextInt();
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for(int i = 0; i < e; i++) {
+            int u = scanner.nextInt();
+            int v = scanner.nextInt();
+            adj.get(u).add(v);
+        }
+        Solution sol = new Solution();
+        List<Integer> res = sol.topologicalSort(adj);
+        for(int i : res) {
+            System.out.print(i + " ");
+        }
+    }
+}*/
