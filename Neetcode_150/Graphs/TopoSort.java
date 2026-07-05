9ed7e5f722f926afa16336b6b5b2b7bf4835aa61
Rishi Das <113520748+Rishi2003Das@users.noreply.github.com>
2026-07-06 01:11:47 +0530

    Update TopoSort.java

--- Neetcode_150/Graphs/TopoSort.java
+++ Neetcode_150/Graphs/TopoSort.java
@@ -51,3 +51,62 @@ class Solution {
         }
     }
 }*/
+
+
+//Kahn's algorithm using BFS
+
+import java.util.*;
+
+public class Solution {
+    public static ArrayList<Integer> topologicalSort(ArrayList<ArrayList<Integer>> adj) {
+        int n = adj.size();
+        List<Integer> indegree = new ArrayList<>(Collections.nCopies(n, 0));
+        Queue<Integer> q = new LinkedList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            for(int k : adj.get(i)) {
+                indegree.set(k, indegree.get(k) + 1);
+            }
+        }
+        for(int i = 0; i < n; i++) {
+            if(indegree.get(i) == 0) {
+                q.add(i);
+            }
+        }
+        while(!q.isEmpty()) {
+            int front = q.poll();
+            ans.add(front);
+
+            for(int k : adj.get(front)) {
+                indegree.set(k, indegree.get(k) - 1);
+                if(indegree.get(k) == 0) {
+                    q.add(k);
+                }
+            }
+        }
+        return ans;
+        
+    }
+}
+
+/*public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int e = sc.nextInt();
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for(int i = 0; i < e; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            adj.get(u).add(v);
+        }
+        Solution sol = new Solution();
+        List<Integer> res = sol.topologicalSort(adj);
+        for(int i : res) {
+            System.out.print(i + " ");
+        }
+    }
+}*/
