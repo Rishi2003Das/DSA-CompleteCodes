d94864933d36dadb371f42186ce5121946a64299
Rishi Das <113520748+Rishi2003Das@users.noreply.github.com>
2026-09-08 00:03:47 +0530

    Add distinct subsequence calculation method
    Implement distinct subsequence calculation with modulo.

--- Neetcode_150/Graphs/Distinct_Sebsequence.java
+++ Neetcode_150/Graphs/Distinct_Sebsequence.java
@@ -57,3 +57,34 @@ class Solution {
         return dp[i][j];
     }
 }
+/* 940. Distinct Subsequence II-
+Given a string s, return the number of distinct non-empty subsequences of s.
+Since the answer may be very large, return it modulo 109 + 7.
+A subsequence of a string is a new string that is formed from the original string 
+by deleting some (can be none) of the characters without disturbing the relative 
+positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not.
+ */
+class Solution {
+    public int distinctSubseqII(String S) {
+        int MOD = 1_000_000_007;
+        int N = S.length();
+        int[] dp = new int[N+1];
+        dp[0] = 1;
+
+        int[] last = new int[26];
+        Arrays.fill(last, -1);
+
+        for (int i = 0; i < N; ++i) {
+            int x = S.charAt(i) - 'a';
+            dp[i+1] = dp[i] * 2 % MOD;
+            if (last[x] >= 0)
+                dp[i+1] -= dp[last[x]];
+            dp[i+1] %= MOD;
+            last[x] = i;
+        }
+
+        dp[N]--;
+        if (dp[N] < 0) dp[N] += MOD;
+        return dp[N];
+    }
+}
