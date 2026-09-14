fe72fc1bc0b149e4b7ed1e5c83d94d4ac615527f
Rishi Das <113520748+Rishi2003Das@users.noreply.github.com>
2026-09-14 14:55:44 +0530

    Create Largest_Good_BinStr.java

---
+++ OA_Questions/Adobe/Largest_Good_BinStr.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+    public String largestMagical(String binString) {
+        int len=binString.length();
+        int prefix[]=new int[len+1];
+        for(int i=0;i<len;i++){
+            
+        }
+        return "";
+    }
+}
