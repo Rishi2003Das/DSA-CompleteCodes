e1b7f04b64c1d732fd51e27082cc6a1c739ee6c6
Rishi Das <113520748+Rishi2003Das@users.noreply.github.com>
2026-09-15 10:50:32 +0530

    Refactor largestMagical to construct good binary string
    Refactor largestMagical method to build and return the largest good binary string.

--- OA_Questions/Adobe/Largest_Good_BinStr.java
+++ OA_Questions/Adobe/Largest_Good_BinStr.java
@@ -3,10 +3,22 @@ import java.util.*;
 public class Solution {
     public String largestMagical(String binString) {
         int len=binString.length();
-        int prefix[]=new int[len+1];
+        int cnt=0;
+        int start=0;
+        List<String> list=new ArrayList<>();
         for(int i=0;i<len;i++){
-            
+            cnt+=(binString.charAt(i)=='1'?1:-1);
+            if(cnt==0){
+                String inner=binString.substring(start+1,i);
+                list.add("1"+largestMagical(inner)+"0");//Recursive function to check the good binString
+                start=i+1;
+            }
         }
-        return "";
+        list.sort((a,b)->(b+a).compareTo(a+b));//sorted the list of substring
+        StringBuilder ans= new StringBuilder();
+        for(String p: list){
+            ans.append(p);
+        }
+        return ans.toString();
     }
 }
