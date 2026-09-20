6072bf8cbee099dbca43cd882d64e5ee202113af
Rishi Das <113520748+Rishi2003Das@users.noreply.github.com>
2026-09-20 14:12:34 +0530

    Create Compatible_Warehouse_Graph.java

---
+++ OA_Questions/Tower Capital/Compatible_Warehouse_Graph.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Solution {
+    public long countCompatiblePairs(int warehouse_nodes, List<Integer> warehouse_from, List<Integer> warehouse_to, List<Integer> warehouse_weight, List<Integer> val) {
+        //Write your code here...
+        
+    }
+}
